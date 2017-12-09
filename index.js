1698690de3128184589740e6cda5ace7f3db9c56
KarolNov <karolnovljakovic@gmail.com>
2017-12-09 22:51:23 +0100

    mobile

--- index.js
+++ index.js
@@ -8,8 +8,12 @@
 //      ajaxStop: function() { $body.removeClass("loading"); }
 // });
 $(document).ready(setTimeout(function () {
+    //welcome screen
     $("body").removeClass("loading");
+    $(".navbar").fadeIn(3000);
+    //scroll event
     $(document).on("scroll", onScroll);
+    //śnieżek
     let d = new Date();
     let m = d.getMonth();
     snowStorm.followMouse = false;
@@ -22,6 +26,13 @@ $(document).ready(setTimeout(function () {
       snowStorm.autoStart = false;
       $('.navbar').css("background-color", "#009444ff");
     }
+    $("#logoM").click(function(){
+      if($('#myNavbar').css("display")=="none"){
+      $('#myNavbar').slideDown(1000);
+    }else{
+      $('#myNavbar').slideUp(1000);
+    }
+    });
     //smoothscroll
     $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
@@ -49,13 +60,19 @@ function onScroll(event){
     if(scrollPos>300){
       $('#menu').fadeOut(1000, "linear");
     }
+    if(scrollPos>350){
+      $('#sidebar').fadeIn(1000, "linear");
+    }
+    if(scrollPos<=350){
+      $('#sidebar').fadeOut(1000, "linear");
+    }
     if(scrollPos<=300){
       $('#menu').fadeIn(1000, "linear");
     }
     $('#menu a').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top-100 <= scrollPos && (refElement.position().top-100 + refElement.height()) > (scrollPos)) {
+        if (Math.abs(refElement.position().top-scrollPos) <= 100 && Math.abs((refElement.position().top + refElement.height())-scrollPos) > 100) {
             $('#menu ul a').removeClass("active");
             currLink.addClass("active");
         }
@@ -111,9 +128,5 @@ function onScroll(event){
       //   $('img[src="./src/kontakt-a.png"]').attr("src", "./src/kontakt.png");
       //   $('img[src="./src/rezerwacja-a.png"]').attr("src", "./src/rezerwacja.png");
     }
-    if($("#logo").hasClass("active")){
-      $('#sidebar').fadeOut(1000, "linear");
-    }else{
-      $('#sidebar').fadeIn(1000, "linear");
-    }
+
 }
